216eb82e06025c136e7363f4aaa62c1c2aa84b0c
rsakhi <sakhi.rohit@gmail.com>
2018-10-22 00:29:14 +0530

    Custom property binding using Input decorators

--- binding/src/app/app.component.ts
+++ binding/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  servers = [{type: "server", name: "default server", content: "first default server"}]
   title = 'binding';
 }
--- binding/src/app/server/server.component.ts
+++ binding/src/app/server/server.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-server',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
-
+  @Input() element = {type: String, name: String, content: String}
   constructor() { }
 
   ngOnInit() {
