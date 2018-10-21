fba02d176d1dc8ce59dfa299eeaa7e53c751b7ed
rsakhi <sakhi.rohit@gmail.com>
2018-10-22 00:08:51 +0530

    created component

--- binding/src/app/app.module.ts
+++ binding/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CockpitComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule
