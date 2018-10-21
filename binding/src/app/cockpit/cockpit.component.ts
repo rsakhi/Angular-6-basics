080357446568aae0b3c994f7f65317afea4a496e
rsakhi <sakhi.rohit@gmail.com>
2018-10-22 00:14:00 +0530

    property and event binding

--- binding/src/app/cockpit/cockpit.component.ts
+++ binding/src/app/cockpit/cockpit.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
 
+  isServerAdded:boolean = false
   constructor() { }
 
   ngOnInit() {
   }
 
+  onServerAdded(){
+    this.isServerAdded = true
+  }
 }
