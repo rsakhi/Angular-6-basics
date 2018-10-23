c94fbd36e0ea81f7f737e257a070c7f5df4e97c2
rsakhi <sakhi.rohit@gmail.com>
2018-10-24 02:16:26 +0530

    Created custom directive

--- directives/src/app/app.module.ts
+++ directives/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
+import { CustomDirective } from './custom.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent
+    DemoComponent,
+    CustomDirective
   ],
   imports: [
     BrowserModule
---
+++ directives/src/app/custom.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, ElementRef, OnInit } from "@angular/core";
+
+@Directive({
+	selector: "[appCustomDirective]"
+})
+
+export class CustomDirective implements OnInit{
+	constructor(private eleRef: ElementRef){
+	}
+
+	ngOnInit(){
+		this.eleRef.nativeElement.style.backgroundColor = "red"
+	}
+}
