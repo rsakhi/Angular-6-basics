90cccfa906aef90f492dae947f192d0fd323fc25
rsakhi <sakhi.rohit@gmail.com>
2018-10-27 00:09:35 +0530

    Adding propertry binding with directives

--- directives/src/app/directive/custom-2.directive.ts
+++ directives/src/app/directive/custom-2.directive.ts
@@ -1,25 +1,28 @@
-import { Directive, ElementRef, Renderer2, OnInit, HostListener, HostBinding } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit, HostListener, HostBinding, Input } from '@angular/core';
+import { HighlightDelayBarrier } from 'blocking-proxy/built/lib/highlight_delay_barrier';
 
 @Directive({
   selector: '[appCustom2]'
 })
 export class Custom2Directive implements OnInit{
-
+  @Input() defaultBgColor: string;
+  @Input() highlightColor: string;
   constructor(private eleRef: ElementRef, private render: Renderer2) { }
-  @HostBinding('style.backgroundColor') bgcolor: string;
+  
+  @HostBinding('style.backgroundColor') bgcolor: string = this.defaultBgColor;
 
   ngOnInit(){
-    this.render.setStyle(this.eleRef.nativeElement, 'background-color', 'blue')
+    // this.render.setStyle(this.eleRef.nativeElement, 'background-color', 'blue')
   }
 
   @HostListener('mouseenter') mouseOver(){
     // this.render.setStyle(this.eleRef.nativeElement, 'background-color', 'blue')
-    this.bgcolor = 'blue'
+    this.bgcolor = this.defaultBgColor
   }
 
   @HostListener('mouseleave') mouseLeave(){
     // this.render.setStyle(this.eleRef.nativeElement, 'background-color', 'transparent')
-    this.bgcolor = 'transparent'
+    this.bgcolor = this.highlightColor;
   }
 
 
